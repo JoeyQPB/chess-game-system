0a9115ea7960b496d94aab38b1843eb790451893
joey <joey_qpb@hotmail.com>
2023-10-07 22:41:29 +1100

    Improve UI

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,6 +5,7 @@ import chess.ChessMatch;
 public class Program {
 	
 	public static void main(String args[]) {
+		System.out.println(" ");
 		
 		ChessMatch chessMatch = new ChessMatch();
 		UI.printBoard(chessMatch.getPieces());
