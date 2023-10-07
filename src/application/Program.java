644589836425ef7ee093a6429d2d6a61ac5b9fe5
joey <joey_qpb@hotmail.com>
2023-10-07 14:28:02 +1100

    feat: Board and Piece class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import boardgame.Board;
 import boardgame.Position;
 
 public class Program {
@@ -7,8 +8,7 @@ public class Program {
 	public static void main(String args[]) {
 		System.out.println("Hello World!!!");
 		
-		Position p = new Position(3 ,5);
-		System.out.print(p);
+		Board board = new Board(8,8);
 	}
 
 }
